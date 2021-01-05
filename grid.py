24fd1b57e38b084fb70d77ec4861720ba8583c63
sudoentropy <sudoentropy@gmail.com>
2021-01-04 22:22:14 -0500

    developing user inputs to grid

---
+++ grid.py
@@ -0,0 +1,14 @@
+# experimenting with logical addtions for user inputs on grid 
+
+
+width = int(input("width: "))
+height = int(input("height: "))
+
+grid2 = []
+
+i = int(0)
+for i in range(width):
+    grid2.append("_")
+for i in range(height):
+    print(grid2)
+
