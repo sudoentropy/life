17f9043acd45d38ec8fd6cadaa47b20677893c29
sudoentropy <sudoentropy@gmail.com>
2021-01-09 15:32:44 -0500

    rendering game board

--- ifname.py
+++ ifname.py
@@ -1,13 +1,15 @@
 # if name == if __name__ == "__main__":
 
 
-def renderStage(sheight, swidht):
+def renderStage(sheight, swidth):
     print("+"+"-"*sheight+"+ ")
 
 
 
 if __name__ == "__main__":
-    print("Hello")
+
+    renderStage(10,10)
+
     inp = input("Add starting blocks: ")
     xstr, ystr = inp.split(",")
     x = int(xstr)
