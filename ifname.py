629eb29a37db320cb346aa41d238cb67a055338c
sudoentropy <sudoentropy@gmail.com>
2021-01-09 23:42:21 -0500

    rendered board and cells

--- ifname.py
+++ ifname.py
@@ -1,19 +1,25 @@
 # if name == if __name__ == "__main__":
 
-
-def renderStage(sheight, swidth):
-    ends = ("+"+"-"*sheight+"+")
+def renderStage(sheight, swidth, seed):
+    ends = "+"+"-"*sheight+"+"
     print(ends)
 
-    for i in range(sheight):
+    for y in range(sheight):
         print("|"+"-"*swidth+"|")
-
+        row = []
+        for x in range(swidth):
+            if (x,y) in seed:
+                cell = "@"
+            else:
+                cell = "-"
+            row.append(cell)
+        print("|"+"".join(row)+"|")
     print(ends)
 
 
 if __name__ == "__main__":
 
-    renderStage(10,10)
+    renderStage(10,10, [(3,1),(4,5),(8,1)])
 
     inp = input("Add starting blocks: ")
     xstr, ystr = inp.split(",")
